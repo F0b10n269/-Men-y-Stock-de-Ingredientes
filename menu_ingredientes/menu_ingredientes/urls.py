ca941c6fbd18f2ea844f1a9c1ab724ecdd567350
F0b10n269 <fabian.calderon07@inacapmail.cl>
2025-10-30 09:26:58 -0300

    Add repository URL comment in urls.py
    Added GitHub repository URL as a comment.

--- menu_ingredientes/menu_ingredientes/urls.py
+++ menu_ingredientes/menu_ingredientes/urls.py
@@ -1,3 +1,6 @@
+# URL del repositorio en GitHub
+repo_url = "https://github.com/F0b10n269/-Men-y-Stock-de-Ingredientes.git"
+
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
@@ -11,4 +14,5 @@ router.register(r'stock', StockViewSet, basename='stock')
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+
 ]

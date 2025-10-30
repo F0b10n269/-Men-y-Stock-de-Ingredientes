4d34741181b311cdece00f79ed7cc423f62c4498
F0b10n269 <fabian.calderon07@inacapmail.cl>
2025-10-30 09:29:25 -0300

    Implement repository cloning in urls.py
    Added functionality to clone the GitHub repository upon running the script.

--- menu_ingredientes/menu_ingredientes/urls.py
+++ menu_ingredientes/menu_ingredientes/urls.py
@@ -1,3 +1,5 @@
+from git import Repo
+
 # URL del repositorio en GitHub
 repo_url = "https://github.com/F0b10n269/-Men-y-Stock-de-Ingredientes.git"
 
@@ -16,3 +18,11 @@ urlpatterns = [
     path('api/', include(router.urls)),
 
 ]
+
+# Carpeta donde quieres clonar el repositorio
+ruta_destino = "C:/Users/lab316.antofagasta/Downloads/menu_ingredientes_clone"
+
+# Clonar el repositorio
+Repo.clone_from(repo_url, ruta_destino)
+
+print("✅ Repositorio clonado correctamente en:", ruta_destino)

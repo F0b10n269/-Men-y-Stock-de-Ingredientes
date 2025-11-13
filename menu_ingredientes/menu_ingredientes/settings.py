d2c5f9d2b08bc28b1e5aaeef11c8bf7272110fde
F0b10n269 <fabian.calderon07@inacapmail.cl>
2025-11-13 09:22:49 -0300

    Update settings.py

--- menu_ingredientes/menu_ingredientes/settings.py
+++ menu_ingredientes/menu_ingredientes/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -130,4 +131,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PARSER_CLASSES': [
         'rest_framework.parsers.JSONParser',
     ],
+
 }

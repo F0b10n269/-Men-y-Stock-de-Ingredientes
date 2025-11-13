1e547153d6aca7182bcc076404f9845ff8162c11
F0b10n269 <fabian.calderon07@inacapmail.cl>
2025-11-13 07:40:29 -0300

    Remove repository cloning code from urls.py
    urls listo para registro de platos

--- menu_ingredientes/menu_ingredientes/urls.py
+++ menu_ingredientes/menu_ingredientes/urls.py
@@ -1,14 +1,9 @@
-from git import Repo
-
-# URL del repositorio en GitHub
-repo_url = "https://github.com/F0b10n269/-Men-y-Stock-de-Ingredientes.git"
-
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from mainApp.views import PlatoViewSet, IngredienteViewSet, StockViewSet
-from .views import PlatoViewSet
 
+#  Configuración correcta del router
 router = DefaultRouter()
 router.register(r'platos', PlatoViewSet, basename='plato')
 router.register(r'ingredientes', IngredienteViewSet, basename='ingrediente')
@@ -18,12 +13,3 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
 ]
-
-# Carpeta donde quieres clonar el repositorio
-ruta_destino = "C:/Users/lab316.antofagasta/Downloads/menu_ingredientes_clone"
-
-# Clonar el repositorio
-Repo.clone_from(repo_url, ruta_destino)
-
-print(" Repositorio clonado correctamente en:", ruta_destino)
-
